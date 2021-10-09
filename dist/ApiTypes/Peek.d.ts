a4973e9ec98f946716bf8605a48c607b47f41958
wanieru <github@wanieru.com>
2021-10-09 17:04:44 +0200

    Update peek request

--- dist/ApiTypes/Peek.d.ts
+++ dist/ApiTypes/Peek.d.ts
@@ -1,10 +1,13 @@
 import * as zod from "zod";
 export declare const peekRequest: zod.ZodObject<{
+    app: zod.ZodString;
     keys: zod.ZodArray<zod.ZodString, "many">;
 }, "strip", zod.ZodTypeAny, {
     keys?: string[];
+    app?: string;
 }, {
     keys?: string[];
+    app?: string;
 }>;
 export declare type PeekRequest = zod.infer<typeof peekRequest>;
 export interface PeekResponse {
--- src/ApiTypes/Peek.ts
+++ src/ApiTypes/Peek.ts
@@ -1,6 +1,7 @@
 import * as zod from "zod";
 export const peekRequest = zod.object(
 {
+    app : zod.string().min(1),
     keys : zod.array(zod.string().min(1)).min(1)
 });
 export type PeekRequest = zod.infer<typeof peekRequest>;
