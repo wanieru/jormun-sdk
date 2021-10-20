3be9fa9c74415bd4896e2246375ee8461d428b54
wanieru <github@wanieru.com>
2021-10-20 23:49:39 +0200

    ok

--- src/Jormun.ts
+++ src/Jormun.ts
@@ -129,8 +129,8 @@ export class Jormun
         {
             const choice = await this.ask("The local and remote data cannot be combined. Which do you want to keep?", 
                 [
-                        `🖥️📤 Local (☁️+${comparison.missingRemote.length + comparison.newerLocal.length}${comparison.missingLocal.length > 0 && `, ☁️-${comparison.missingLocal.length}`})`, 
-                        `☁️📥 Remote (🖥️+${comparison.missingLocal.length + comparison.newerRemote.length}${comparison.missingRemote.length > 0 && `, 🖥️-${comparison.missingRemote.length}`})`, 
+                        `🖥️ Local (${(comparison.missingRemote.length + comparison.newerLocal.length) > 0 ? `☁️ +${comparison.missingRemote.length + comparison.newerLocal.length}`:""}${comparison.missingLocal.length > 0 ? `, ☁️ -${comparison.missingLocal.length}`:""})`, 
+                        `☁️ Remote (${(comparison.missingLocal.length + comparison.newerRemote.length) > 0 ? `🖥️ +${comparison.missingLocal.length + comparison.newerRemote.length}`:""}${comparison.missingRemote.length > 0 ? `, 🖥️ -${comparison.missingRemote.length}`:""})`, 
                         "Cancel"
                 ]);
             if(choice == 0)
