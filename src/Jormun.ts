f595d872718f36bce75b3d6d399ee870d3aa9880
wanieru <github@wanieru.com>
2021-10-20 23:32:05 +0200

    Simpler descriptions simply describing the amount of changes.

--- src/Jormun.ts
+++ src/Jormun.ts
@@ -129,8 +129,8 @@ export class Jormun
         {
             const choice = await this.ask("The local and remote data cannot be combined. Which do you want to keep?", 
                 [
-                        `Local (⬆️${comparison.missingRemote.length + comparison.newerLocal.length} 🗑️${comparison.missingLocal.length} 🔗${comparison.deleteShared.length + comparison.newShared.length})`, 
-                        `Remote (⬇️${comparison.missingLocal.length + comparison.newerRemote.length} 🗑️${comparison.missingRemote.length} 🔗${comparison.deleteShared.length + comparison.newShared.length})`, 
+                        `Local (↑${comparison.missingRemote.length + comparison.newerLocal.length + comparison.missingLocal.length} )`, 
+                        `Remote (↓${comparison.missingLocal.length + comparison.newerRemote.length + comparison.missingRemote.length}})`, 
                         "Cancel"
                 ]);
             if(choice == 0)
